0a9db985152f766c9bd578b19974c9015dc0866f
Sarah Depew <star.1776@gmail.com>
2018-02-08 12:44:21 -0500

    Through Task 2

--- src/Lab03.java
+++ src/Lab03.java
@@ -10,13 +10,18 @@ public class Lab03 {
 		
 		//Initialize Array
 		initialize(A, arraySize); 
-		printArray(A, arraySize); 
+		//printArray(A, arraySize); 
 		
 		//Shuffle Array Randomly 
 		FisherYatesShuffle(A, arraySize);
+		//printArray(A, arraySize); 
+		
+		quicksort(A, 0, arraySize-1); 
 		printArray(A, arraySize); 
+		
 	}
 	
+	//Task #1
 	private static void initialize(int[] A, int n) {
 		for(int i=0; i<n; i++){
 			A[i] = i; 
@@ -28,7 +33,7 @@ public class Lab03 {
 		int j; 
 		
 		for(int i=0; i<n-1; i++) {
-			j = random.nextInt(n-i+1)+i; //get the random number j such that i<=j<n
+			j = random.nextInt(n-i)+i; //get the random number j such that i<=j<n
 			int temp = A[i]; 
 			A[i] = A[j]; 
 			A[j] = temp; 
@@ -36,9 +41,50 @@ public class Lab03 {
 	}
 	
 	private static void printArray(int[] A, int n) {
+		System.out.print("{");
 		for(int i=0; i<n; i++) {
 			int valueToPrint = A[i]; 
-			System.out.println(valueToPrint);
+			System.out.print(valueToPrint + ", ");
+		}
+		System.out.println("}");
+	}
+	
+	//Task #2
+	private static void quicksort(int[] A, int left, int right) {
+		Random random = new Random(); 
+		int m = left;
+		
+		if(left>=right) {
+			return; 
+		}
+		
+		//pivot is a random element in A[l]...A[u]
+		swap(A, left, random.nextInt(right-left+1)+left); 
+					
+		for(int i=left+1; i<=right; i++) {
+			if(A[i]<A[left]) {
+				swap(A, ++m, i);
+			}
+		}
+		
+		swap(A, left, m); 
+		quicksort(A, left, m-1); 
+		quicksort(A, m+1, right); 	
+	}
+	
+	private static void swap(int A[], int i, int j) {
+		int temp = A[j]; 
+		A[j] = A[i]; 
+		A[i] = temp; 
+	}
+	
+	private static void insertionSort(int[] A, int n) {
+		int key; 
+		int j; 
+		
+		for(int i=1; i<n; i++) {
+			key = A[i]; 
+			
 		}
 	}
 }
